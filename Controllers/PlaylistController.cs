00b5901ecd68fafb847f35797bc20afb8c0e8764
Özgür Işık Çimen <50545611+26518304542@users.noreply.github.com>
2023-01-12 21:05:32 +0300

    Add files via upload

---
+++ Controllers/PlaylistController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson.Serialization.Attributes;
+using RestfulAPImongodb.Models;
+using RestfulAPImongodb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPImongodb.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+
+    public class PlaylistController : Controller
+    {
+
+        private readonly MongoDBService _mongoDBService;
+
+        public PlaylistController(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        [HttpGet]
+        public async Task<List<Playlist>> Get()
+        {
+            return await _mongoDBService.GetAsync();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Playlist playlist)
+        {
+            await _mongoDBService.CreateAsync(playlist);
+            return CreatedAtAction(nameof(Get), new { id = playlist.Id }, playlist);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddToPlaylist( string id,[FromBody] string movieID) {
+
+            await _mongoDBService.AddToPlaylistAsync(id, movieID);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id) {
+            await _mongoDBService.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/Playlist.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RestfulAPImongodb.Models
+{
+    public class Playlist
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string username { get; set; } = null;
+        [BsonElement("items")]
+        [JsonPropertyName("items")]
+        public List<string> movieIds { get; set; } = null;
+    }
+}
